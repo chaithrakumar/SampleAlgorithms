248c3d13c27a9b4cfa909c07b9e26acc74425a7e
chaithra Kumar <chaithra.kumar@erosnow.com>
2017-01-04 20:40:15 -0800

    BST deleting a node

--- BinarySearchTree.playground/Contents.swift
+++ BinarySearchTree.playground/Contents.swift
@@ -39,7 +39,6 @@ class BinarySearchTree {
     }
     //Function to insert an item in to the tree
     private func insertNum(inputData : Int, atNode inputNode:Node) {
-        
         let node = Node(inputData: inputData)
         if inputData <= inputNode.data! {
             if inputNode.leftNode != nil {
@@ -201,15 +200,84 @@ class BinarySearchTree {
         
     }
     
+    func FindMin(rootNode : Node?) -> Int? {
+        
+        if rootNode == nil
+        {
+            return nil
+        }
+        if rootNode?.leftNode == nil {
+            return rootNode?.data
+        }
+       return FindMin(rootNode: rootNode?.leftNode)
+    }
+    
+    
+    
+    func Delete( prootNode :Node? , pdata : Int) -> Node?
+    {
+        // if its a empty tree return nil
+        var mutablerootnode = prootNode
+        
+        if mutablerootnode == nil
+        {
+            return nil
+        }
+        
+        if (mutablerootnode?.data)! < pdata {
+            
+            mutablerootnode?.rightNode = Delete(prootNode: mutablerootnode?.rightNode, pdata: pdata)
+            
+            
+        } else if (mutablerootnode?.data)! > pdata {
+            
+            mutablerootnode?.leftNode = Delete(prootNode: mutablerootnode?.leftNode, pdata: pdata)
+            
+        } else {
+            //case 1 
+            if mutablerootnode?.leftNode == nil && mutablerootnode?.rightNode == nil {
+                mutablerootnode = nil
+                
+            }
+            
+            //case 2
+            if mutablerootnode?.leftNode == nil {
+                mutablerootnode = mutablerootnode?.rightNode
+            }
+            if mutablerootnode?.rightNode == nil {
+                mutablerootnode = mutablerootnode?.leftNode
+            }
+            //case 3 
+            let mindata = FindMin(rootNode: mutablerootnode)
+            mutablerootnode?.data = mindata
+            mutablerootnode?.leftNode = Delete(prootNode: mutablerootnode?.leftNode, pdata: mindata!)
+            
+            
+        }
+        return mutablerootnode
+        
+        
+    }
     
     
     
     
+    func printvalues(rootNode : Node?) {
+        
+        if rootNode == nil {
+            return
+        }
+        
+        print(rootNode?.data ?? 0)
+        printvalues(rootNode: rootNode?.leftNode)
+        printvalues(rootNode: rootNode?.rightNode)
+        
+        
+    }
     
     
     //print all right and left node values of BST
     func printAllValues() {
-        
         var node = self.rootNode
         repeat {
             print(node?.data ?? 0)
@@ -234,13 +302,18 @@ tree.insertItem(num: 41)
 tree.insertItem(num: 42)
 tree.insertItem(num: 43)
 tree.insertItem(num:20)
-//tree.printAllValues()
+tree.printvalues(rootNode: tree.rootNode)
+tree.Delete(prootNode: tree.rootNode, pdata: 40)
+tree.Delete(prootNode: tree.rootNode, pdata: 43)
+print("--------")
+tree.printvalues(rootNode: tree.rootNode)
+
 //tree.levelOrderTraversal()
-tree.performPreOrderTraversal(root: tree.rootNode)
-print("--------")
-tree.performInOrderTraversal(root: tree.rootNode)
-print("--------")
-tree.performPostOrderTraversal(root: tree.rootNode)
+//tree.performPreOrderTraversal(root: tree.rootNode)
+//print("--------")
+//tree.performInOrderTraversal(root: tree.rootNode)
+//print("--------")
+//tree.performPostOrderTraversal(root: tree.rootNode)
 
 //let height = tree.findheightOfNode(inputNode:tree.rootNode)
 //print(height)
